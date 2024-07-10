8c71511c52f8e64d8a49c47ad16cd4124073648d
Wael Abidi <113802588+weedii@users.noreply.github.com>
2024-07-10 13:40:19 +0100

    Add files via upload

---
+++ Client/src/Pages/Home.jsx
@@ -0,0 +1,15 @@
+import Header from "../Components/Header";
+import About from "../Components/About";
+import ExploreCatalog from "../Components/ExploreCatalog";
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <ExploreCatalog />
+      <About />
+    </div>
+  );
+};
+
+export default Home;
