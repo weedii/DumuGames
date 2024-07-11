06aae55696061822cdd08b3b7c8176acda94ce94
Wael Abidi <113802588+weedii@users.noreply.github.com>
2024-07-11 02:54:21 +0100

    Delete Client directory

--- Client/src/PrivateRoutes.jsx
+++
@@ -1,9 +0,0 @@
-import { useSelector } from "react-redux";
-import { Outlet, Navigate } from "react-router-dom";
-
-const PrivateRoutes = () => {
-  const currentUser = useSelector((state) => state.currentUser.user);
-  return currentUser ? <Outlet /> : <Navigate to="/sign-in" replace />;
-};
-
-export default PrivateRoutes;
